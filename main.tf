14a8f7bb491812ab1bce565180160c035a48b32e
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-13 12:10:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 provider "aws" {
 region = "us-east-1"
+aws_access_key = var.accesskey
+   secret_access_key = var.secretaccesskey
 }
  resource "aws_instance" "harness" {
   # The ID of the EC2 instance that you want to import
    instance_type = "t2.micro"  
    ami = "ami-08b5b3a93ed654d19"
-   aws_access_key = var.accesskey
-   secret_access_key = var.secretaccesskey
+   
 tags = {
     Name = "harness"
 }
