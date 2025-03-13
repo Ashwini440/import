f53c99f485f957daecbc700cb39c30cdfc30b274
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-13 13:02:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,14 @@ terraform {
 
 provider "aws" {
   region      = "us-east-1"
-  access_key  = var.accesskey
-  secret_key  = var.secretaccesskey
-  //access_key  = "<+pipeline.variables.accesskey>"
-  //secret_key  = "<+pipeline.variables.secretaccesskey>"
+  access_key  = "<+pipeline.variables.accesskey>"
+  secret_key  = "<+pipeline.variables.secretaccesskey>"
 }
 
 resource "aws_instance" "harness" {
   instance_type = "t2.micro"
   ami           = "ami-08b5b3a93ed654d19"
-  instance_id   = var.instance_id
+  instance_id   = "<+pipeline.variables.instance_id>"
   
   tags = {
     Name = "harness"
@@ -32,6 +30,7 @@ variable "accesskey" {
 variable "secretaccesskey" {
   type = string
 }
+
 variable "instance_id" {
-type = string
+  type = string
 }
